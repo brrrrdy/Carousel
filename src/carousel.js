4119fa4920d652d71674e372cee3550af1d9fdef
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2025-07-24 16:28:40 +0200

    setting up carousel behaviour

---
+++ src/carousel.js
@@ -0,0 +1,15 @@
+/*
+
+export carousel
+
+CAROUSEL BEHAVIOUR:
+
+- The carousel should automatically cycle through images every 5 seconds.
+- Users should be able to pause the automatic cycling by hovering over the carousel.
+- Users should be able to manually navigate through the images using next and previous buttons.
+- It should contain arrows on each side to advance the image forward or backward. 
+-  It should contain the little navigation circles at the bottom that indicate which slide you are on (and they should be clickable to advance to that particular slide).
+- The carousel should display a set of images that can be defined in the JavaScript file.
+- The carousel should be responsive and adapt to different screen sizes.
+
+*/
